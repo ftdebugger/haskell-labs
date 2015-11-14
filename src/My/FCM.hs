887bd59695fe592ae4a863d20e45b6c22c5f7845
Evgeny Shpilevsky <evgeny@shpilevsky.com>
2015-11-14 22:47:28 +0300

    Clusterize method

--- src/My/FCM.hs
+++ src/My/FCM.hs
@@ -1,18 +1,32 @@
 module My.FCM where
 
-import qualified Data.Vector as V
 import System.Random
+import Control.Monad
+import Data.List
 
-type Distance = V.Vector Double -> V.Vector Double -> Double
-type Points = [Float]
+type Points = [Double]
 type Matrix = [Points]
 
+type Object = [Double]
+type Objects = [Object]
+
+type Accesorry = Double
+type Accessories = [Accesorry]
+
+type Center = Object
+type Centers = [Center]
+
+type Cluster = (Center, Objects)
+type Clusters = [Cluster]
+
+type Distance = Object -> Object -> Double
+
 euclidDistance :: Distance
-euclidDistance v1 v2 = sqrt $ V.sum $ V.zipWith diff v1 v2
+euclidDistance v1 v2 = sqrt $ sum $ zipWith diff v1 v2
   where diff a b = (a-b)^(2::Int)
 
 hammingDistance :: Distance
-hammingDistance v1 v2 = V.sum $ V.zipWith diff v1 v2
+hammingDistance v1 v2 = sum $ zipWith diff v1 v2
   where diff a b = abs $ a-b
 
 generateLine :: Int -> IO Points
@@ -26,21 +40,66 @@ generateLine n = do
 generateAccessory :: Int -> -- N
                      Int -> -- C
                      IO Matrix
-generateAccessory n c = mapM generateLine (replicate c n)
+generateAccessory n c = replicateM c (generateLine n)
 
--- generateAccessory n 1 = do
---   line <- generateLine n
---   return [line]
--- generateAccessory n c = do
---   line <- generateLine n
---   matrix <- generateAccessory n (c - 1)
---
---   return line:matrix :: IO Matrix
+zeroVector :: Int -> [Double]
+zeroVector n = replicate n 0
 
-fcm :: Distance -> -- Distnace function (euclid or hamming)
-       Int -> -- number of clusters
-       Float -> -- accuracy
-       Matrix -> -- accessory matrix
-       Matrix -> -- matrix with N number vecotrs
-       Matrix --  result matrix with acces
-fcm distance clustersCount eps accessory matrix = [[]]
+accessoryDistance :: Distance ->
+                  Center ->
+                  Object -> -- accessory
+                  Accesorry ->
+                  Double
+accessoryDistance distance center obj accessory = accessory * accessory * dist
+  where dist = distance center obj
+
+accessoriesDistance :: Distance ->
+                      Center ->
+                      Objects ->
+                      Accessories ->
+                      Double
+accessoriesDistance distance center objects accessories = sum $ map dist zipDist
+  where zipDist = zip objects accessories
+        dist (obj, accessory) = accessoryDistance distance center obj accessory
+
+randInRange :: Int -> Int -> IO Int
+randInRange a b = getStdRandom $ randomR (a, b)
+
+randomPick :: [a] -> IO a
+randomPick xs = liftM (xs !!) (randomRIO (0, length xs - 1))
+
+-- getRandomCenters :: Int -> -- Clucsters
+--                     Objects ->
+--                     IO Centers
+getRandomCenters :: Int -> Objects -> IO Objects
+getRandomCenters c objects = replicateM c (pick c)
+  where pick _ = randomPick objects
+
+cluserize :: Distance -> Centers -> Objects -> [Cluster]
+cluserize distance centers objects = createClusters getCenters
+  where createClusters objs = map (createCluster objs) centers
+        createCluster objs center = (center, pickWithCenter center objs)
+        pickWithCenter center objs = map first $ filter (filterWithCenter center) objs
+        filterWithCenter center (_, a) = a == center
+
+        getCenters = map getCenter objects
+        getCenter object = (object, first $ minCenter $ distances object)
+        distances object = map (dist object) centers
+        dist object center = (center, distance object center)
+        minCenter = minimumBy cmp
+        cmp a b = compare (second a) (second b)
+        second (_, numDist) = numDist
+        first (center, _) = center
+
+-- calculateCenters :: Int -> -- number of clusters
+--                     Matrix -> -- input matrix
+--                     Matrix -> -- accessory matrix
+--                     Points -- cetners
+
+-- fcm :: Distance -> -- Distnace function (euclid or hamming)
+--        Int -> -- number of clusters
+--        Float -> -- accuracy
+--        Matrix -> -- accessory matrix
+--        Matrix -> -- matrix with N number vectors
+--        Matrix --  result matrix with acces
+-- fcm distance clustersCount eps accessory matrix = [[]]
--- test/My/FCMSpec.hs
+++ test/My/FCMSpec.hs
@@ -1,7 +1,6 @@
 module My.FCMSpec where
 
 import My.FCM
-import qualified Data.Vector as V
 
 import Test.Hspec
 
@@ -9,14 +8,14 @@ spec :: Spec
 spec = do
   describe "FCM Distance" $ do
     it "calculate euclid distance" $ do
-      let v1 = V.fromList [1, 2, 3]
-      let v2 = V.fromList [1, 5, 7]
+      let v1 = [1, 2, 3]
+      let v2 = [1, 5, 7]
 
       euclidDistance v1 v2 `shouldBe` 5
 
     it "calculate hamming distance" $ do
-      let v1 = V.fromList [1, 2, 3]
-      let v2 = V.fromList [1, 5, 7]
+      let v1 = [1, 2, 3]
+      let v2 = [1, 5, 7]
 
       hammingDistance v1 v2 `shouldBe` 7
 
@@ -26,4 +25,41 @@ spec = do
       sum line `shouldBe` 1
     it "generate random matrix" $ do
       matrix <- generateAccessory 3 2
-      (sum $ map sum matrix) `shouldBe` 2
+      sum (map sum matrix) `shouldBe` 2
+
+  describe "Utils" $ do
+    it "generate zero vector" $
+      zeroVector 5 `shouldBe` [0, 0, 0, 0, 0]
+
+    it "calculate accessory distance with euclid" $ do
+      let v1 = [1, 2, 3]
+      let v2 = [1, 5, 7]
+      let dist = accessoryDistance euclidDistance v1 v2 0.1
+
+      getRandomCenters 2 [[1], [2], [3]] >>= print
+
+      abs ( dist - 0.05 ) < 0.0001 `shouldBe` True
+
+    it "calculate accessory distance with hamming" $ do
+      let v1 = [1, 2, 3]
+      let v2 = [1, 5, 7]
+      let dist = accessoryDistance hammingDistance v1 v2 0.1
+
+      abs ( dist - 0.07 ) < 0.0001 `shouldBe` True
+
+    it "calculate accessories distance with euclid" $ do
+      let v1 = [1, 2, 3]
+      let v2 = [[1, 5, 7], [1, 5, 7]]
+      let acc = [0.1, 0.5]
+
+      let dist = accessoriesDistance euclidDistance v1 v2 acc
+
+      dist `shouldBe` 1.3
+
+  describe "Clusterize" $
+    it "should create correct clusters" $ do
+      let objects = [[1, 2, 3], [1, 3, 4],[1, 1, 5],[1, 5, 6]]
+      let centers = [[1, 2, 3], [1, 3, 4]]
+
+      let clusters = cluserize euclidDistance centers objects
+      print clusters
